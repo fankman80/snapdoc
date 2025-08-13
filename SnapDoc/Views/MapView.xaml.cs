76d8de48f84296c078ece01ec23620d99d006c00
fankman80 <m.fankhauser@gmx.net>
2025-08-13 18:10:22 +0200

    Update MapView.xaml.cs

--- SnapDoc/Views/MapView.xaml.cs
+++ SnapDoc/Views/MapView.xaml.cs
@@ -147,6 +147,9 @@ public partial class MapView : IQueryAttributable
 
     private static string Generatescript()
     {
+        if (GlobalJson.Data.Plans != null)
+            return;
+    
         string positionsJson = "[";
         foreach (var plan in GlobalJson.Data.Plans)
         {
