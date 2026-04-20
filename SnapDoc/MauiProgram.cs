4fcf4dc7b1db0684874fd03c178189f4a1e2e4f3
fankman80 <m.fankhauser@gmx.net>
2026-04-20 09:21:44 +0200

    Bugfixes 20.04.2026

--- SnapDoc/MauiProgram.cs
+++ SnapDoc/MauiProgram.cs
@@ -30,7 +30,12 @@ public static class MauiProgram
             .UseSkiaSharp()
             .UseSentry(options =>
             {
+                // DSN nur setzen, wenn wir NICHT im Debug sind
+#if DEBUG
+                options.Dsn = "";
+#else
                 options.Dsn = "https://b864c3fdd54cf3fe92c37b849cb6e9cd@o4511245885308928.ingest.de.sentry.io/4511245957267536";
+#endif
                 options.TracesSampleRate = 1.0;
                 options.EnableLogs = true;
                 options.AttachScreenshot = true;
