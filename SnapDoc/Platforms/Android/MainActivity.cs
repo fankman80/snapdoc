2d49f0ee67387de540bcf669bdd31a9fff5f9f83
fankman80 <m.fankhauser@gmx.net>
2026-04-28 13:50:31 +0200

    Refactor MainActivity by removing unused code
    Removed Instance property and OnCreate window color settings.

--- SnapDoc/Platforms/Android/MainActivity.cs
+++ SnapDoc/Platforms/Android/MainActivity.cs
@@ -11,31 +11,9 @@ namespace SnapDoc.Platforms.Android
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        public static MainActivity? Instance { get; private set; }
-
         protected override void OnCreate(Bundle? savedInstanceState)
         {
-            Instance = this;
             base.OnCreate(savedInstanceState);
-
-            if (Window != null)
-            {
-                WindowCompat.SetDecorFitsSystemWindows(Window, false);
-                Window.SetStatusBarColor(global::Android.Graphics.Color.Transparent);
-            }
-        }
-        public void UpdatePlatformColors(bool isLightStatus)
-        {
-            if (Window == null) return;
-
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                var decorView = Window.DecorView;
-                if (decorView == null) return;
-
-                var controller = WindowCompat.GetInsetsController(Window, decorView);
-                controller?.AppearanceLightStatusBars = isLightStatus;
-            });
         }
     }
 }
