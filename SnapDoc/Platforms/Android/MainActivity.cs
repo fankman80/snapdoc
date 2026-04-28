a8728165a90a2286538169234a2962e7e142b535
fankman80 <m.fankhauser@gmx.net>
2026-04-28 12:58:47 +0200

    Bugfixes 28.04.2026

--- SnapDoc/Platforms/Android/MainActivity.cs
+++ SnapDoc/Platforms/Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.View;
 
 namespace SnapDoc.Platforms.Android
 {
@@ -10,9 +11,31 @@ namespace SnapDoc.Platforms.Android
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public static MainActivity? Instance { get; private set; }
+
         protected override void OnCreate(Bundle? savedInstanceState)
         {
+            Instance = this;
             base.OnCreate(savedInstanceState);
+
+            if (Window != null)
+            {
+                WindowCompat.SetDecorFitsSystemWindows(Window, false);
+                Window.SetStatusBarColor(global::Android.Graphics.Color.Transparent);
+            }
+        }
+        public void UpdatePlatformColors(bool isLightStatus)
+        {
+            if (Window == null) return;
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                var decorView = Window.DecorView;
+                if (decorView == null) return;
+
+                var controller = WindowCompat.GetInsetsController(Window, decorView);
+                controller?.AppearanceLightStatusBars = isLightStatus;
+            });
         }
     }
 }
