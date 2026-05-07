eade2f9a827b01bb8b8469f4a1ddc2654c5940e5
fankman80 <m.fankhauser@gmx.net>
2026-05-07 14:47:27 +0200

    Bugfixes 07.05.2026

--- SnapDoc/Platforms/Android/MainActivity.cs
+++ SnapDoc/Platforms/Android/MainActivity.cs
@@ -1,13 +1,14 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace SnapDoc.Platforms.Android
 {
     [Activity(Theme = "@style/Maui.SplashTheme",
               MainLauncher = true,
               LaunchMode = LaunchMode.SingleTop,
-              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, WindowSoftInputMode = SoftInput.AdjustResize)]
     public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
