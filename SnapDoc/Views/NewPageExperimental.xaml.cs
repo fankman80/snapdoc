b8023b13aaaa2308709fc0adbc73b46613c6cf32
fankman80 <m.fankhauser@gmx.net>
2026-07-12 00:35:50 +0200

    Adjust drawing coordinates for display density

--- SnapDoc/Views/NewPageExperimental.xaml.cs
+++ SnapDoc/Views/NewPageExperimental.xaml.cs
@@ -953,8 +953,8 @@ public partial class NewPageExperimental : IQueryAttributable, INotifyPropertyCh
             await StartDrawing(false);
 
             drawingController.LoadFromFile(filePath, new SKPoint(
-                (float)(this.Width / 2 + SettingsService.Instance.CustomPinOffset.X),
-                (float)(this.Height / 2 + SettingsService.Instance.CustomPinOffset.Y)));
+                (float)((this.Width / 2 + SettingsService.Instance.CustomPinOffset.X) * Settings.DisplayDensity),
+                (float)((this.Height / 2 + SettingsService.Instance.CustomPinOffset.Y) * Settings.DisplayDensity)));
 
             drawingController.ViewRotation = thisPlan.Pins[tappedPin.Id].PinRotation - drawingController.InitialRotation;
 
