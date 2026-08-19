6e1b96c79d32df46f5a76d802980aeddca26f51e
fankman80 <m.fankhauser@gmx.net>
2026-08-20 00:27:46 +0200

    Add MsalActivity for MSAL authentication

---
+++ SnapDoc/Platforms/Android/MsalActivity.cs
@@ -0,0 +1,14 @@
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+
+namespace SnapDoc.Platforms.Android;
+
+[Activity(Exported = true, LaunchMode = LaunchMode.SingleTask, NoHistory = true, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+[IntentFilter(new[] { Intent.ActionView },
+    Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
+    DataScheme = "msal00fdac1d-aa0a-49c1-a238-a46a88f69ce6",
+    DataHost = "auth")]
+public class MsalActivity : Microsoft.Identity.Client.BrowserTabActivity
+{
+}
