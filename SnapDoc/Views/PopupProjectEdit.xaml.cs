9f779b1316488e9225404d7f36cf7472f4e51c33
fankman80 <m.fankhauser@gmx.net>
2026-09-17 19:04:21 +0200

    Refactor PopupProjectEdit to remove entry handling
    Removed entry parameter and related logic from PopupProjectEdit constructor and OnOkClicked method.

--- SnapDoc/Views/PopupProjectEdit.xaml.cs
+++ SnapDoc/Views/PopupProjectEdit.xaml.cs
@@ -7,26 +7,14 @@ namespace SnapDoc.Views;
 
 public partial class PopupProjectEdit : Popup<string>
 {
-    private readonly string _entry;
-
-    public PopupProjectEdit(string entry, bool isActive = false, string okText = null, string cancelText = null)
+    public PopupProjectEdit(bool isActive = false, string okText = null, string cancelText = null)
     {
         InitializeComponent();
         okButtonText.Text = okText ?? AppResources.ok;
         cancelButtonText.Text = cancelText ?? AppResources.abbrechen;
-        text_entry.Text = entry;
-
-        _entry = entry;
-
         UploadButton.IsVisible = SettingsService.Instance.IsCloudLoggedIn && isActive;
     }
-
-    private async void OnOkClicked(object sender, EventArgs e)
-    {
-        try { await CloseAsync(_entry != text_entry.Text ? text_entry.Text : null); }
-        catch (InvalidOperationException) { }
-    }
-
+    
     private async void OnCancelClicked(object sender, EventArgs e)
     {
         try { await CloseAsync(null); }
